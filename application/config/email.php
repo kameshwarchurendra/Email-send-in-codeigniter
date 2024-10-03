b786dba5e1c9db8101dece07689c772d5b509bbc
kameshwar churendra <79463893+kameshwarchurendra@users.noreply.github.com>
2024-10-03 06:30:39 +0100

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@
 $config['protocol']  = 'smtp';
 $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_port'] = 587;
-$config['smtp_user'] = 'kameshwarchurendra1995@gmail.com';
-$config['smtp_pass'] = 'tmddxfhekzeuabrw';
+$config['smtp_user'] = 'example@gmail.com';
+$config['smtp_pass'] = 'XXXXXXXXXXXXX';
 $config['smtp_crypto'] = 'tls'; // Enable TLS
 $config['mailtype']  = 'html';
 $config['charset']   = 'utf-8';
